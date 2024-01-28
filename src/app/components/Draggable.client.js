c1b41edd126e51d6fb9647b894632a02b6a74650
taki dilmi <angeldilmi@gmail.com>
2024-01-28 16:54:07 +0100

    initialize app, and create a draggable component

---
+++ src/app/components/Draggable.client.js
@@ -0,0 +1,61 @@
+'use client';
+import React from 'react';
+
+const Draggable = () => {
+  const dragStart = (event) => {
+    event.dataTransfer.setData('text/plain', event.target.id);
+  };
+
+  const allowDrop = (event) => {
+    event.preventDefault();
+  };
+
+  const drop = (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('text/plain');
+    const element = document.getElementById(data);
+    const clonedElement = element.cloneNode(true);
+    clonedElement.style.position = 'absolute';
+    clonedElement.style.left = `${event.clientX}px`;
+    clonedElement.style.top = `${event.clientY}px`;
+    if (data === 'myImage') {
+      clonedElement.textContent = '';
+      const img = document.createElement('img');
+      img.src = 'https://via.placeholder.com/200';
+      clonedElement.appendChild(img);
+    }
+    event.target.appendChild(clonedElement);
+  };
+
+  return (
+    <>
+      <div className="flex w-screen h-screen">
+        <div className="flex-1 bg-red-600">
+          <p
+            id="myImage"
+            draggable="true"
+            onDragStart={dragStart}
+          >
+            Image
+          </p>
+          <p
+            id="myText"
+            draggable="true"
+            onDragStart={dragStart}
+          >
+            draggable
+          </p>
+        </div>
+        <div
+          className="flex-1 bg-blue-600"
+          onDrop={drop}
+          onDragOver={allowDrop}
+        >
+          Right Side
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Draggable;
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Draggable from './components/Draggable.client';
+
+
+const page = () => {
+  return (
+    <div>
+      <Draggable />
+    </div>
+  );
+};
+
+export default page;
